0f2a9a31b82c8121287f95ee638d1d8e4a5f6637
Pablo Lopez Domowicz <pldomowicz@gmail.com>
2014-11-10 09:55:39 -0500

    added provisioning profile

--- Chainer/Chainer/AppDelegate.swift
+++ Chainer/Chainer/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application( application: UIApplication!, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData! ) {
-        println(deviceToken)
+//        println(deviceToken)
         var characterSet: NSCharacterSet = NSCharacterSet( charactersInString: "<>" )
         var deviceTokenString: String = ( deviceToken.description as NSString )
             .stringByTrimmingCharactersInSet( characterSet )
