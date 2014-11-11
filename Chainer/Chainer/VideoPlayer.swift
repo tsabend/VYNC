d2045585ad1ac4caa14a312caf6161c12c291703
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-11-11 16:23:56 -0500

    remove extra println from videplayer library

--- Chainer/Chainer/VideoPlayer.swift
+++ Chainer/Chainer/VideoPlayer.swift
@@ -12,10 +12,7 @@ import AVFoundation
 import Foundation
 
 public func avPlayerControllerFor(url: String) -> AVPlayerViewController {
-    // create player controller
-//    let testUrl = "https://s3-us-west-2.amazonaws.com/telephono/395998122c2e7ba0e06ce82a71dac347.mov?"
     let player: AVPlayer = AVPlayer(URL: NSURL(string: url))
-
     // create player view controller
     let avPlayerVC = AVPlayerViewController()
     avPlayerVC.player = player
@@ -29,12 +26,7 @@ public func playVidUrlOnViewController(vidUrl: String, vc: UIViewController) {
     
     // show player view controller
     vc.presentViewController(avPlayerVC, animated: true, completion: {
-        // start playing
-        println("playing \(avPlayerVC.player.currentItem)")
-        println("status before play = \(avPlayerVC.player.status.rawValue)")
         avPlayerVC.player.play()
-         println("status after play = \(avPlayerVC.player.status.rawValue)")
-        
     })
     
 }
