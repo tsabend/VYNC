11ec2e0eca118915d7b043e38eddb8f2c9c49ed9
Faraaz Nishtar & Thomas Abend <pair+imuzz.tsabend@devbootcamp.com>
2014-11-13 14:22:34 -0500

    launch screen

--- Chainer/Chainer/MainViewController.swift
+++ Chainer/Chainer/MainViewController.swift
@@ -91,12 +91,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             cell.addSubview(button)
             cell.textLabel.text = "\(sendingUser!)"
             cell.detailTextLabel?.text = "\(chains[indexPath.row].count) \(link) long. \(stringDate)"
+            cell.imageView.image = UIImage(contentsOfFile :"/Users/apprentice/Downloads/new.png")
         } else if sentID == userID.toInt()! {                                           // if you sent the message
             cell.textLabel.text = "Following"
             cell.detailTextLabel?.text = "\(chains[indexPath.row].count) \(link) long. \(stringDate)"
+            cell.imageView.image = UIImage(contentsOfFile :"/Users/apprentice/Downloads/new.png")
         } else {                                                        // if you are just following
             cell.textLabel.text = "Following \(sendingUser!)"
             cell.detailTextLabel?.text = "\(chains[indexPath.row].count) \(link) long. \(stringDate)"
+            cell.imageView.image = UIImage(contentsOfFile :"/Users/apprentice/Downloads/new.png")
         }
         return cell
     }
