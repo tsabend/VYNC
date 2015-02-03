69ea73eb67a16689872513e1c39545d60b56b598
Pablo Lopez Domowicz <pldomowicz@gmail.com>
2015-02-03 10:15:43 -0600

    fully functional fb login

---
+++ VYNC/VYNC/Bridge-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridge-Header.h
+//  VYNC
+//
+//  Created by Pablo Lopez Domowicz on 2/3/15.
+//  Copyright (c) 2015 Thomas Abend. All rights reserved.
+//
+
+#ifndef VYNC_Bridge_Header_h
+#define VYNC_Bridge_Header_h
+
+#import <FacebookSDK/FacebookSDK.h>
+
+#endif
