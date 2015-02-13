66970569ff130810194fb13f6a00dd8bd1795392
tsabend <tsabend@gmail.com>
2015-02-13 10:46:13 -0500

    Add code to force deinit of preview layer. Add spinning animation

--- VYNC/VYNC/VyncCameraViewController.swift
+++ VYNC/VYNC/VyncCameraViewController.swift
@@ -19,19 +19,24 @@ class VyncCameraViewController: UIViewController, AVCaptureFileOutputRecordingDe
     var previewLayer : AVCaptureVideoPreviewLayer!
     var captureMovieFileOutput: AVCaptureMovieFileOutput? = nil;
     var videoConnection : AVCaptureConnection!
-    
     var vync : Vync!
-    
+    var rotating = false
+    var playerLayerView : VyncCameraPlaybackLayer!
     @IBOutlet weak var flashButton: UIButton!
     
+    @IBOutlet weak var flipCameraButton: UIButton!
     @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         setupCamera()
-        
+//        let flash = NSString(
+        flashButton.setTitle("\u{e001}", forState: .Normal)
+        flipCameraButton.setTitle("\u{e003}", forState: .Normal)
+        recordButton.setTitle("\u{e000}", forState: .Normal)
         let backToVyncView = UIScreenEdgePanGestureRecognizer(target: self, action: "dismissCamera:")
         backToVyncView.edges = UIRectEdge.Left
         self.view.addGestureRecognizer(backToVyncView)
@@ -84,12 +89,12 @@ class VyncCameraViewController: UIViewController, AVCaptureFileOutputRecordingDe
             if captureDevice.torchMode.hashValue == 0 {
                 captureDevice.lockForConfiguration(nil)
                 captureDevice.torchMode = AVCaptureTorchMode.Auto
-                flashButton.setTitle("off", forState: .Normal)
+                flashButton.setTitle("\u{e002}", forState: .Normal)
                 captureDevice.unlockForConfiguration()
             } else {
                 captureDevice.lockForConfiguration(nil)
                 captureDevice.torchMode = AVCaptureTorchMode.Off
-                flashButton.setTitle("flash", forState: .Normal)
+                flashButton.setTitle("\u{e001}", forState: .Normal)
                 captureDevice.unlockForConfiguration()
             }
         }
@@ -98,13 +103,13 @@ class VyncCameraViewController: UIViewController, AVCaptureFileOutputRecordingDe
     @IBAction func flipCamera(sender: AnyObject) {
         println("flipping camera")
         captureSession.beginConfiguration()
-        if let currentCamera = captureSession.inputs.first as? AVCaptureDeviceInput {
+        if let currentCamera = captureSession.inputs.last as? AVCaptureDeviceInput {
             if currentCamera.device.position == AVCaptureDevicePosition.Back {
-                captureSession.removeInput(captureSession.inputs.first as! AVCaptureInput)
+                captureSession.removeInput(captureSession.inputs.last as! AVCaptureInput)
                 captureSession.addInput(AVCaptureDeviceInput(device: selfieCaptureDevice, error: nil))
                 flashButton.hidden = true
             } else {
-                captureSession.removeInput(captureSession.inputs.first as! AVCaptureInput)
+                captureSession.removeInput(captureSession.inputs.last as! AVCaptureInput)
                 captureSession.addInput(AVCaptureDeviceInput(device: captureDevice, error: nil))
                 flashButton.hidden = false
             }
@@ -112,7 +117,36 @@ class VyncCameraViewController: UIViewController, AVCaptureFileOutputRecordingDe
         captureSession.commitConfiguration()
     }
     
+    func beginSession() {
+        var err : NSError? = nil
+        captureSession.addInput(AVCaptureDeviceInput(device: audioCaptureDevice, error: &err))
+        captureSession.addInput(AVCaptureDeviceInput(device: captureDevice, error: &err))
+        let tap = UITapGestureRecognizer(target:self, action:"onTap:")
+        self.view.addGestureRecognizer(tap)
+        if err != nil {
+            println("error: \(err?.localizedDescription)")
+        }
+        
+        var previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer?.frame = UIScreen.mainScreen().bounds
+        previewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        self.view.layer.insertSublayer(previewLayer, atIndex: 0)
+        captureSession.startRunning()
+    }
     
+    @IBAction func startRecording(sender: AnyObject) {
+        println("startRecording")
+        let fileUrl = NSURL.fileURLWithPath(pathToFile) as NSURL!
+        captureMovieFileOutput!.startRecordingToOutputFileURL(fileUrl, recordingDelegate: self)
+        rotating = true
+        rotateOnce()
+    }
+    
+    @IBAction func stopRecording(sender: AnyObject) {
+        println("endRecording")
+        rotating = false
+        captureMovieFileOutput?.stopRecording()
+    }
     
     func captureOutput(captureOutput: AVCaptureFileOutput!,
         didStartRecordingToOutputFileAtURL fileURL: NSURL!,
@@ -124,53 +158,34 @@ class VyncCameraViewController: UIViewController, AVCaptureFileOutputRecordingDe
         fromConnections connections: [AnyObject]!,
         error: NSError!) {
             println("playing back video")
-            let playerLayerView = VyncCameraPlaybackLayer.loadFromNib() as! VyncCameraPlaybackLayer
+            playerLayerView = VyncCameraPlaybackLayer.loadFromNib() as! VyncCameraPlaybackLayer
             playerLayerView.videoList = [outputFileURL, outputFileURL]
             playerLayerView.playbackDelegate = self
             playerLayerView.playVideos()
             self.view.addSubview(playerLayerView)
     }
     
-    @IBAction func startRecording(sender: AnyObject) {
-        println("startRecording")
-        let fileUrl = NSURL.fileURLWithPath(pathToFile) as NSURL!
-        captureMovieFileOutput!.startRecordingToOutputFileURL(fileUrl, recordingDelegate: self)
-        self.recordButton.setTitle("Recording", forState: .Normal)
-    }
     
-    @IBAction func stopRecording(sender: AnyObject) {
-        println("endRecording")
-        self.recordButton.setTitle("RECORD", forState: .Normal)
-        captureMovieFileOutput?.stopRecording()
-    }
-    
-    @IBAction func dismissCamera(sender:UIScreenEdgePanGestureRecognizer) {
-        if sender.state == .Ended {
-            println("dismissing camera")
-            self.dismissViewControllerAnimated(false, completion:{finished in
-                UIApplication.sharedApplication().setStatusBarHidden(false, withAnimation: .None)})
+    @IBAction func dismissCamera(sender: AnyObject) {
+        if let gesture = sender as? UIScreenEdgePanGestureRecognizer {
+            if gesture.state == .Ended {
+                transitionToRootView()
+            }
+        } else {
+                transitionToRootView()
         }
     }
     
-    
-    func beginSession() {
-        var err : NSError? = nil
-        captureSession.addInput(AVCaptureDeviceInput(device: captureDevice, error: &err))
-        captureSession.addInput(AVCaptureDeviceInput(device: audioCaptureDevice, error: &err))
-        let tap = UITapGestureRecognizer(target:self, action:"onTap:")
-        self.view.addGestureRecognizer(tap)
-        if err != nil {
-            println("error: \(err?.localizedDescription)")
+    func transitionToRootView(){
+        if self.playerLayerView != nil {
+            self.playerLayerView.removeFromSuperview()
+            // This is necessary because if there is still
+            // a pointer to the playerLayer, it won't deinit
+            self.playerLayerView = nil
         }
-        
-        var previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        
-        previewLayer?.frame = UIScreen.mainScreen().bounds
-        previewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
-        self.view.layer.insertSublayer(previewLayer, atIndex: 0)
-        captureSession.startRunning()
+        let vc = self.storyboard?.instantiateViewControllerWithIdentifier("RootNavigationController") as! UINavigationController
+        presentViewController(vc, animated: false, completion: {})
     }
-    
     
     func onTap (tap: UITapGestureRecognizer){
         if let device = captureDevice {
@@ -182,25 +197,39 @@ class VyncCameraViewController: UIViewController, AVCaptureFileOutputRecordingDe
     }
     // Playback Delegate Methods
     func retakeVideo(view:VyncCameraPlaybackLayer) {
+        self.playerLayerView = nil
         view.removeFromSuperview()
+        
     }
     
     func acceptVideo(view:VyncCameraPlaybackLayer) {
         view.removeFromSuperview()
         if (self.vync != nil) {
-            println("reply")
             let contactsNav = self.storyboard?.instantiateViewControllerWithIdentifier("ContactsNav") as! UINavigationController
             let contacts = contactsNav.viewControllers[0] as! ContactsViewController
-            // TODO: replace this code with the actual replyToID that has been passed around
             contacts.replyToId = vync.replyToId()
             self.presentViewController(contactsNav, animated: false, completion: nil)
         } else {
-            println("first")
             let title = self.storyboard?.instantiateViewControllerWithIdentifier("TitleNav") as! UINavigationController
             self.presentViewController(title, animated: false, completion: nil)
         }
     }
     
+    func rotateOnce() {
+        UIView.animateWithDuration(0.5,
+            delay: 0.0,
+            options: .CurveLinear,
+            animations: {self.recordButton.transform = CGAffineTransformRotate(self.recordButton.transform, 3.1415926)},
+            completion: {finished in self.rotateAgain()})
+    }
     
+    func rotateAgain() {
+        UIView.animateWithDuration(0.5,
+            delay: 0.0,
+            options: .CurveLinear,
+            animations: {self.recordButton.transform = CGAffineTransformRotate(self.recordButton.transform, 3.1415926)},
+            completion: {finished in if self.rotating { self.rotateOnce() }})
+    }
+
 }
 
