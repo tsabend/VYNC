74c2cd069cdb73adeed76b891c267e3412120ffe
tsabend <tsabend@gmail.com>
2015-02-24 18:23:05 -0500

    Refactored name method to avoid release error

--- VYNC/VYNC/LoadFromNibExtension.swift
+++ VYNC/VYNC/LoadFromNibExtension.swift
@@ -9,10 +9,10 @@ import Foundation
 import UIKit
 
 public func getName(classType:AnyClass) -> String {
-    
     let classString = NSStringFromClass(classType)
-    let range = classString.rangeOfString(".", options: NSStringCompareOptions.CaseInsensitiveSearch, range: Range<String.Index>(start:classString.startIndex, end: classString.endIndex), locale: nil)
-    return classString.substringFromIndex(range!.endIndex)
+    let swiftClassString = "\(classString)"
+    let arr : [String] = swiftClassString.componentsSeparatedByString(".")
+    return arr.last!
 }
 
 extension UIView {
